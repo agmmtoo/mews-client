1a9c8c09abeb3be4db5ac95cca5e65ac2912efb9
Aung Myint Myat Oo <solidifyarmor@gmail.com>
2022-04-20 16:36:22 +0630

    dark mode

--- src/components/MewsRead.js
+++ src/components/MewsRead.js
@@ -42,14 +42,14 @@ export default MewsRead;
 const MewsDetail = ({ mews }) => {
     let ms = new Date(mews.createdAt) / 1000
     return (
-        <div className='rounded-lg shadow-md mx-auto mt-5 p-3 w-11/12 md:w-4/5 bg-gradient-to-r from-fuchsia-300 to-indigo-300'>
+        <div className='rounded-lg shadow-md mx-auto mt-5 p-3 w-11/12 md:w-4/5 bg-gradient-to-r from-fuchsia-300 to-indigo-300 dark:from-violet-400 dark:to-fuchsia-400'>
             {mews.title && <h1 className='font-semibold text-2xl'>{mews.title}</h1>}
-            <div className='flex flex-row items-center gap-5 text-sm text-slate-500'>
+            <div className='flex flex-row items-center gap-5 text-sm'>
                 <div className=''>@{mews.submitter.username}</div>
                 <div>{formatTime(ms)}</div>
             </div>
             {mews.body && <p className='pt-3 mt-3 border-t-2 border-t-inherit'>{mews.body}</p>}
-            <div className="pt-3 mt-3 flex flex-row items-center gap-5 text-slate-700 border-t-2 border-t-inherit">
+            <div className="pt-3 mt-3 flex flex-row items-center gap-5 border-t-2 border-t-inherit">
                 <div className='flex items-center gap-2'>
                     <svg className='' fill='currentColor' width='17' height='17' viewBox="0 0 320 512">
                         <path d="M296 160H180.6l42.6-129.8C227.2 15 215.7 0 200 0H56C44 0 33.8 8.9 32.2 20.8l-32 240C-1.7 275.2 9.5 288 24 288h118.7L96.6 482.5c-3.6 15.2 8 29.5 23.3 29.5 8.4 0 16.4-4.4 20.8-12l176-304c9.3-15.9-2.2-36-20.7-36z" />
